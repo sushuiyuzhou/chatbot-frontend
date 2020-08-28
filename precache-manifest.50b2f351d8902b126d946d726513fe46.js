2a0b7726fe9cdf3d60f9142985840f40f00e7855
sushuiyuzhou <colin.chuang.sun@gmail.com>
2020-08-28 12:06:05 +0100

    Updates

---
+++ precache-manifest.50b2f351d8902b126d946d726513fe46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08cb36c73f0d79ceb1d4931017b4011c",
+    "url": "/chatbot/index.html"
+  },
+  {
+    "revision": "ae0ef6af972b8c6b8a54",
+    "url": "/chatbot/static/js/2.296a5ffa.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/chatbot/static/js/2.296a5ffa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbc8a1ae7d6a47adcf6e",
+    "url": "/chatbot/static/js/main.3d56c32c.chunk.js"
+  },
+  {
+    "revision": "6ae62d57f813ef07a157",
+    "url": "/chatbot/static/js/runtime-main.8c55ad46.js"
+  },
+  {
+    "revision": "84ff5913f9560cfb72118344d53ed5cb",
+    "url": "/chatbot/static/media/rain.84ff5913.jpg"
+  }
+]);
