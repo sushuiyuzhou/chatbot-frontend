c76c78f96f15a8fd092d0b7e96c11087343f1548
sushuiyuzhou <colin.chuang.sun@gmail.com>
2020-08-30 22:46:01 +0100

    change to valid domain

--- src/config.js
+++ src/config.js
@@ -1,2 +1,3 @@
 // export const apiHost = "http://localhost:8888/";
-export const apiHost = "https://122.51.183.191:8888/";
+// export const apiHost = "https://122.51.183.191:80/";
+export const apiHost = "https://soulin.io:80/";
